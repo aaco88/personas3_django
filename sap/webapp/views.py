01cdb0a349ce5eadf0219b59993374b2a4ea2707
Albert <albert.@mail.com>
2022-06-19 22:11:56 -0300

    Creando app personas y migraciones

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from personas.models import Persona
+
+
+def bienvenido(request):
+    cantPersonas = Persona.objects.count()
+    return render(request, 'bienvenido.html', {'cPersonas': cantPersonas})
