d504fda765a8df65597066a83ee1d282e5d706b1
Albert <albert.@mail.com>
2022-06-19 23:08:04 -0300

    Editar y Eliminar Persona terminado

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from personas.models import Persona
+from personas.forms import PersonaForm
 
 
 def detallePersona(request, id):
@@ -11,7 +12,7 @@ def detallePersona(request, id):
     return render(request, 'persona/detalle.html', {'dPersona': detPersona})
 
 
-PersonaForm = modelform_factory(Persona, exclude=[])
+# PersonaForm = modelform_factory(Persona, exclude=[])
 
 
 def agregarPersona(request):
@@ -23,3 +24,22 @@ def agregarPersona(request):
     else:
         formaPersona = PersonaForm()
     return render(request, 'persona/agregar.html', {'fPersona': formaPersona})
+
+
+def editarPersona(request, id):
+    editPersona = get_object_or_404(Persona, pk=id)
+    if request.method == 'POST':
+        formaPersona = PersonaForm(request.POST, instance=editPersona)
+        if formaPersona.is_valid():
+            formaPersona.save()
+            return redirect('index')
+    else:
+        formaPersona = PersonaForm(instance=editPersona)
+    return render(request, 'persona/editar.html', {'fPersona': formaPersona})
+
+
+def eliminarPersona(request, id):
+    elimPersona = get_object_or_404(Persona, pk=id)
+    if elimPersona:
+        elimPersona.delete()
+    return redirect('index')
--- sap/sap/urls.py
+++ sap/sap/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path
 
 from webapp.views import bienvenido
-from personas.views import detallePersona, agregarPersona
+from personas.views import detallePersona, agregarPersona, editarPersona, eliminarPersona
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', bienvenido, name='index'),
     path('detalle_persona/<int:id>', detallePersona),
-    path('agregar_persona', agregarPersona)
+    path('agregar_persona', agregarPersona),
+    path('editar_persona/<int:id>', editarPersona),
+    path('eliminar_persona/<int:id>', eliminarPersona)
 ]
